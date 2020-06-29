36939638658d18f34e2d5c2fed1695151dac0686
Shawn Cicoria <github@cicoria.com>
2020-06-29 18:12:31 -0400

    m2 refactor to class

--- notebooks/project/m1.py
+++ notebooks/project/m1.py
@@ -21,100 +21,99 @@
 # that one has to traverse to go from t1 to t2. (Hint: Use function
 # NumberToCoordinate for this)
 
-import os
-import pathlib
+class IslandProject():
 
-cur_dir = pathlib.Path(__file__).parent.absolute()
-file_name = os.path.join(cur_dir, 'shawn-cicoria.txt')
+    def __init__(self, m, n):
+        self.m = m
+        self.n = n
 
-
-m = 10
-n = 10
-
-
-def load_file(file_name):
-    """load file to an array line by line"""
-    rv = []
-    with open(file_name) as f:
-        line = list(f.readline().strip())
-        while line:
-            rv.append(line)
+    def load_file(self, file_name):
+        """load file to an array line by line"""
+        rv = []
+        with open(file_name) as f:
             line = list(f.readline().strip())
+            while line:
+                rv.append(line)
+                line = list(f.readline().strip())
 
-    return rv
+        self.contents = rv
+        return self.contents
 
+    def find_land_cells(self):
+        """returns the points that have land markers"""
+        """if the point has a land marker return it"""
+        rv = []
+        for r, i in enumerate(range(self.n)):
+            for c, j in enumerate(range(self.m)):
+                if self.contents[i][j] == '1':
+                    rv.append([i, j])
 
-def find_land_cells(data, m, n):
-    """returns the points that have land markers"""
-    """if the point has a land marker return it"""
-    rv = []
-    for r, i in enumerate(range(n)):
-        for c, j in enumerate(range(m)):
-            if data[i][j] == '1':
-                rv.append([i, j])
+        self.land_cell_list = rv
+        return self.land_cell_list
 
-    return rv
+    def coordinate_to_number(self, i, j):
+        """ takes a coordinate (i, j) and"""
+        """maps it to a unique number t in [0, mn − 1]"""
+        """"which is then returned by the function."""
+        rv = i * self.n + j
+        return rv
 
+    def number_to_coordinate(self, t):
+        i = t // self.n
+        j = t % self.n
+        return i, j
 
-def coordinate_to_number(i, j, m, n):
-    """ takes a coordinate (i, j) and"""
-    """maps it to a unique number t in [0, mn − 1]"""
-    """"which is then returned by the function."""
-    rv = i * n + j
-    return rv
+    def distance(self, t1, t2):
+        i1, j1 = self.number_to_coordinate(t1)
+        i2, j2 = self.number_to_coordinate(t2)
 
+        rv = abs(i1 - i2) + abs(j1 - j2)
+        return rv
 
-def number_to_coordinate(t, m, n):
-    i = t // n
-    j = t % n
-    return i, j
+    #     0    1    2    3    4    5    6    7    8    9
+    # 0 ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
+    # 1 ['0', '1', '1', '0', '0', '0', '1', '0', '0', '0']
+    # 2 ['0', '0', '0', '0', '0', '0', '1', '0', '0', '0']
+    # 3 ['0', '0', '0', '1', '1', '0', '1', '0', '0', '0']
+    # 4 ['0', '0', '0', '1', '0', '0', '0', '0', '0', '0']
+    # 5 ['0', '0', '0', '1', '0', '0', '0', '0', '0', '0']
+    # 6 ['0', '0', '0', '1', '1', '0', '0', '0', '0', '0']
+    # 7 ['0', '0', '1', '0', '0', '0', '0', '0', '0', '0']
+    # 8 ['0', '0', '1', '0', '0', '0', '0', '1', '1', '1']
+    # 9 ['0', '0', '1', '0', '0', '0', '0', '0', '0', '0']
 
+    def generate_neighbors(self, x, y):
+        """should return number of neighbors for the item"""
+        """ can be 0, 2, 3, 4 """
+        result = []
+        # this 'mess' finds the items on outside of the x,y
+        for r, c in [(x + i, y + j)
+                     for i in (-1, 0, 1)  # rows over/under
+                     for j in (-1, 0, 1)  # cols left/right
+                     if i != 0 or j != 0]:  # all but THIS cell
 
-def distance(t1, t2, m=3, n=5):
-    i1, j1 = number_to_coordinate(t1, m, n)
-    i2, j2 = number_to_coordinate(t2, m, n)
+            if [r, c] in self.land_cell_list:
+                rv = self.coordinate_to_number(r, c)
+                result.append(rv)
 
-    rv = abs(i1 - i2) + abs(j1 - j2)
+        return result
 
-    return rv
+    def explore_island(self, t1):
+        result = []
 
-#     0    1    2    3    4    5    6    7    8    9
-# 0 ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-# 1 ['0', '1', '1', '0', '0', '0', '1', '0', '0', '0']
-# 2 ['0', '0', '0', '0', '0', '0', '1', '0', '0', '0']
-# 3 ['0', '0', '0', '1', '1', '0', '1', '0', '0', '0']
-# 4 ['0', '0', '0', '1', '0', '0', '0', '0', '0', '0']
-# 5 ['0', '0', '0', '1', '0', '0', '0', '0', '0', '0']
-# 6 ['0', '0', '0', '1', '1', '0', '0', '0', '0', '0']
-# 7 ['0', '0', '1', '0', '0', '0', '0', '0', '0', '0']
-# 8 ['0', '0', '1', '0', '0', '0', '0', '1', '1', '1']
-# 9 ['0', '0', '1', '0', '0', '0', '0', '0', '0', '0']
-
-
-def generate_neighbors(land_cells_list, x, y, m, n):
-    """should return number of neighbors for the item"""
-    """ can be 0, 2, 3, 4 """
-    result = []
-    # this 'mess' finds the items on outside of the x,y
-    for r, c in [(x + i, y + j)
-                 for i in (-1, 0, 1)  # rows over/under
-                 for j in (-1, 0, 1)  # cols left/right
-                 if i != 0 or j != 0]:  # all but THIS cell
-
-        if [r, c] in land_cells_list:
-            rv = coordinate_to_number(r, c, m, n)
-            result.append(rv)
-
-    return result
-
-
-def explore_island():
-    pass
+        return result
 
 
 if __name__ == '__main__':
-    contents = load_file(file_name)
-    land_cell_list = find_land_cells(contents, m, n)
+    import os
+    import pathlib
+    cur_dir = pathlib.Path(__file__).parent.absolute()
+    file_name = os.path.join(cur_dir, 'shawn-cicoria.txt')
+
+    m, n = 10, 10
+    p = IslandProject(m, n)
+    contents = p.load_file(file_name)
+    land_cell_list = p.find_land_cells()
 
     print('file contents: \n{}'.format(contents))
     print('\nland points: \n{}'.format(land_cell_list))
--- notebooks/project/test_m1.py
+++ notebooks/project/test_m1.py
@@ -2,75 +2,123 @@ import unittest
 import os
 import pathlib
 
-from m1 import (load_file,
-                coordinate_to_number,
-                number_to_coordinate,
-                distance,
-                find_land_cells,
-                generate_neighbors)
+from m1 import IslandProject
 
 
 class m1_part_1(unittest.TestCase):
     def setUp(self):
         cur_dir = pathlib.Path(__file__).parent.absolute()
         file_name = os.path.join(cur_dir, 'shawn-cicoria.txt')
-        self.contents = load_file(file_name)
+        self.p = IslandProject(3, 5)
+        self.contents = self.p.load_file(file_name)
 
     def test_one(self):
         self.assertGreater(len(self.contents), 0, 'file has contents')
 
     def test_coord_to_number1(self):
-        n = coordinate_to_number(0, 0, 2, 5)
+        n = self.p.coordinate_to_number(0, 0)
         self.assertEqual(n, 0, '[0,0] is at 0')
 
     def test_coord_to_number_0_5(self):
-        n = coordinate_to_number(0, 4, 2, 5)
+        n = self.p.coordinate_to_number(0, 4)
         self.assertEqual(n, 4, '[0,4] is at 4')
 
     def test_coord_to_number_1_0(self):
-        n = coordinate_to_number(1, 0, 2, 5)
+        n = self.p.coordinate_to_number(1, 0)
         self.assertEqual(n, 5, '[1,0] is at 5')
 
     def test_coord_to_number_1_1(self):
-        n = coordinate_to_number(1, 1, 2, 5)
+        n = self.p.coordinate_to_number(1, 1)
         self.assertEqual(n, 6, '[1,1] is at 6')
 
     def test_coord_to_number_2_1(self):
-        n = coordinate_to_number(2, 1, 2, 5)
+        n = self.p.coordinate_to_number(2, 1)
         self.assertEqual(n, 11, '[2,1] is at 11')
 
     def test_coord_to_number_2_4(self):
-        n = coordinate_to_number(2, 4, 3, 5)
+        n = self.p.coordinate_to_number(2, 4)
         self.assertEqual(n, 14, '[2, 4] is at 14')
 
     def test_number_to_coord_14(self):
         tv = 14
-        i, j = number_to_coordinate(tv, 3, 5)
+        i, j = self.p.number_to_coordinate(tv)
         self.assertEqual([i, j], [2, 4], '{} begets 2,4'.format(tv))
 
     def test_number_to_coord_11(self):
         tv = 11
-        i, j = number_to_coordinate(tv, 3, 5)
+        i, j = self.p.number_to_coordinate(tv)
         self.assertEqual([i, j], [2, 1], '{} begets 2,1'.format(tv))
 
 
 class m1_part_2(unittest.TestCase):
+    def setUp(self):
+        self.p = IslandProject(3, 5)
+
     def test_distance_1(self):
-        t1 = coordinate_to_number(0, 0, 3, 5)
-        t2 = coordinate_to_number(0, 1, 3, 5)
+        t1 = self.p.coordinate_to_number(0, 0)
+        t2 = self.p.coordinate_to_number(0, 1)
         d = 1
 
-        rv = distance(t1, t2)
+        rv = self.p.distance(t1, t2)
         self.assertEqual(d, rv, 'incorrect distance')
 
     def test_distance_2(self):
-        t1 = coordinate_to_number(0, 0, 3, 5)
-        t2 = coordinate_to_number(1, 1, 3, 5)
+        t1 = self.p.coordinate_to_number(0, 0)
+        t2 = self.p.coordinate_to_number(1, 1)
         d = 2
 
-        rv = distance(t1, t2)
+        rv = self.p.distance(t1, t2)
         self.assertEqual(d, rv, 'incorrect distance')
 
+
+class m2_part_1(unittest.TestCase):
+    def setUp(self):
+        m, n = 10, 10
+        self.p = IslandProject(m, n)
+        cur_dir = pathlib.Path(__file__).parent.absolute()
+        self.file_name = os.path.join(cur_dir, 'shawn-cicoria.txt')
+        self.contents = self.p.load_file(self.file_name)
+        self.land_cell_list = self.p.find_land_cells()
+
+    def test_find_neighbor_1(self):
+        """should return number of neighbors for the item"""
+        a1 = self.p.coordinate_to_number(1, 2)
+        r2 = self.p.generate_neighbors(1, 1)
+
+        self.assertIn(a1, r2, 'did not find neighbor')
+
+    def test_find_neighbor_2(self):
+        """should return number of neighbors for the item"""
+        a1 = self.p.coordinate_to_number(2, 6)
+        r2 = self.p.generate_neighbors(1, 6)
+
+        self.assertIn(a1, r2, 'did not find neighbor')
+
+    def test_find_neighbor_3(self):
+        """should return number of neighbors for the item"""
+        a1 = self.p.coordinate_to_number(1, 6)
+        a2 = self.p.coordinate_to_number(3, 6)
+        r2 = self.p.generate_neighbors(2, 6)
+
+        self.assertIn(a1, r2, 'a1 did not find neighbor')
+        self.assertIn(a2, r2, 'a2 did not find neighbor')
+
+    def test_find_neighbor_4(self):
+        """should return number of neighbors for the item"""
+        a1 = self.p.coordinate_to_number(8, 8)
+        r2 = self.p.generate_neighbors(8, 9)
+
+        self.assertIn(a1, r2, 'a1 did not find neighbor')
+
+
+class m2_part_4(unittest.TestCase):
+    def setUp(self):
+        m, n = 10, 10
+        self.p = IslandProject(m, n)
+        cur_dir = pathlib.Path(__file__).parent.absolute()
+        self.file_name = os.path.join(cur_dir, 'shawn-cicoria.txt')
+        self.contents = self.p.load_file(self.file_name)
+        self.land_cell_list = self.p.find_land_cells()
 
 #     0    1    2    3    4    5    6    7    8    9
 # 0 ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
@@ -83,47 +131,16 @@ class m1_part_2(unittest.TestCase):
 # 7 ['0', '0', '1', '0', '0', '0', '0', '0', '0', '0']
 # 8 ['0', '0', '1', '0', '0', '0', '0', '1', '1', '1']
 # 9 ['0', '0', '1', '0', '0', '0', '0', '0', '0', '0']
-class m2_part_1(unittest.TestCase):
-    def setUp(self):
-        m, n = 10, 10
-        cur_dir = pathlib.Path(__file__).parent.absolute()
-        self.file_name = os.path.join(cur_dir, 'shawn-cicoria.txt')
-        self.contents = load_file(self.file_name)
-        self.land_cell_list = find_land_cells(self.contents, m, n)
 
-    def test_find_neighbor_1(self):
-        """should return number of neighbors for the item"""
-        m, n = 10, 10
-        a1 = coordinate_to_number(1, 2, m, n)
-        r2 = generate_neighbors(self.land_cell_list, 1, 1, m, n)
+    def test_explore_island_1(self):
+        t1 = self.p.coordinate_to_number(1, 1)
 
-        self.assertIn(a1, r2, 'did not find neighbor')
+        r2 = self.p.generate_neighbors(1, 1)
+        self.assertEqual(len(r2), 1)
 
-    def test_find_neighbor_2(self):
-        """should return number of neighbors for the item"""
-        m, n = 10, 10
-        a1 = coordinate_to_number(2, 6, m, n)
-        r2 = generate_neighbors(self.land_cell_list, 1, 6, m, n)
-
-        self.assertIn(a1, r2, 'did not find neighbor')
-
-    def test_find_neighbor_3(self):
-        """should return number of neighbors for the item"""
-        m, n = 10, 10
-        a1 = coordinate_to_number(1, 6, m, n)
-        a2 = coordinate_to_number(3, 6, m, n)
-        r2 = generate_neighbors(self.land_cell_list, 2, 6, m, n)
-
-        self.assertIn(a1, r2, 'a1 did not find neighbor')
-        self.assertIn(a2, r2, 'a2 did not find neighbor')
-
-    def test_find_neighbor_4(self):
-        """should return number of neighbors for the item"""
-        m, n = 10, 10
-        a1 = coordinate_to_number(8, 8, m, n)
-        r2 = generate_neighbors(self.land_cell_list, 8, 9, m, n)
-
-        self.assertIn(a1, r2, 'a1 did not find neighbor')
+        rv = self.p.explore_island(t1)
+        self.assertIsNotNone(rv)
+        
 
 if __name__ == '__main__':
     unittest.main()
