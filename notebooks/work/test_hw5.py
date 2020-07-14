635367cb9d15a697f8c0b031f2d733713c1cec75
Shawn Cicoria <github@cicoria.com>
2020-07-13 20:02:52 -0400

    working union.find

--- notebooks/work/test_hw5.py
+++ notebooks/work/test_hw5.py
@@ -3,27 +3,26 @@
 
 import unittest
 
-#  TODO: weight and compression additions
-
 class UnionFind():
     def __init__(self, N):
         self._id = list(range(N))
         self._sz = [1] * N  # size in num of node children
 
     def find(self, p: int, q: int) -> bool:
-        # return self._id[p] == self._id[q]
         return self.root(p) == self.root(q)
 
     def root(self, i: int) -> int:
-        while i != self._id[i]:
-            self._id[i] = self._id[self._id[i]]
-            i = self._id[i]
-        return i
+        j = i
+        while (j != self._id[j]):
+            self._id[j] = self._id[self._id[j]]
+            j = self._id[j]
+        return j
 
     def unite(self, p: int, q: int):
         i = self.root(p)
         j = self.root(q)
-        self._id[i] = j
+        if i == j:
+            return
         if self._sz[i] < self._sz[j]:
             self._id[i] = j
             self._sz[j] += self._sz[i]
@@ -44,21 +43,6 @@ class basic_tests(unittest.TestCase):
         self.assertEqual(uf.find(5, 5), True)
         self.assertEqual(uf.find(5, 4), False)
 
-    def test_root_1(self):
-        uf = UnionFind(10)
-        iv = [0, 0, 0, 1, 1, 1, 3, 3, 6, 6]
-        exp = [0, 0, 0, 0, 1, 1, 0, 3, 6, 0]
-
-        uf._id = iv
-
-        self.assertEqual(len(uf._id), 10)
-        self.assertEqual(len(exp), 10)
-
-        #  uf.unite(9, 4)
-        rv = uf.root(9)
-        self.assertEqual(rv, 0, 'root not match')
-        self.assertListEqual(exp, uf._id)
-
     def test_unite_1(self):
         iv = [0, 1, 2, 3, 3, 5, 6, 7, 8, 9]
         exp = [0, 1, 2, 3, 3, 5, 6, 7, 8, 3]
@@ -70,12 +54,18 @@ class basic_tests(unittest.TestCase):
 
         self.assertListEqual(uf._id, exp)
 
-#  Implement a function randperm that takes as input a number  n,
-#  and returns a random permutation of the numbers 0...n-1.
-#  This was covered in lecture 7. Your implementation should use  O(1)
-#  space in addition to the space needed for the output
-#  (Note: you can use any random number generator functions from
-#  Python's random module, but you have to give you own implementation for randperm)
+    def test_all_1(self):
+        uf = UnionFind(10)
+        #  use the sample from orig pdf on p33 https://www.cs.princeton.edu/~rs/AlgsDS07/01UnionFind.pdf
+        for (p, q) in [(3, 4), (4, 9), (8, 0), (2, 3), (5, 6), (5, 9), (7, 3), (4, 8), (6, 1)]:
+            uf.unite(p, q)
+
+        self.assertListEqual(uf._id, [8, 3, 3, 3, 3, 3, 3, 3, 3, 3])
+
+        rv = uf.find(0, 1)
+        self.assertTrue(rv)
+        self.assertListEqual(uf._id, [3, 3, 3, 3, 3, 3, 3, 3, 3, 3])
+
 
 def randperm(n, seed=42):
     import random
@@ -101,7 +91,7 @@ class test_rand_stuff(unittest.TestCase):
     def test_list_diff(self):
         act = randperm(10)
         exp = list(range(10))
-        
+
         self.assertNotEqual(act, exp, 'list is equal')
 
         #  check if every element in act is in exp
